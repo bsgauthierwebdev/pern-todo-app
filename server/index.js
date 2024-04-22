87196bce8c630e97420378716655ea48249f2cd1
Brent Gauthier <cajundrummer@gmail.com>
2024-04-22 09:19:40 -0400

    Import db connection information to allow for routing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const pool = require('./db')
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// ROUTES //
+
+// Create a ToDo
+
+// Get All ToDos
+
+// Update a ToDo
+
+// Delete a ToDo
 
 app.listen(5000, () => {
     console.log('Server is now listening on port 5000')
